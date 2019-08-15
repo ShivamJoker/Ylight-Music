8d444a70fa3d660cdcbd5d93ebb2c7f81ad42031
Shivam <singhshivamkr@gmail.com>
2019-08-15 21:27:04 +0530

    feedback form improved with progress

--- src/components/sections/FeedbackForm.js
+++ src/components/sections/FeedbackForm.js
@@ -1,13 +1,24 @@
 import React from "react";
-import { FormControl, TextField, Button, Grid } from "@material-ui/core";
+import {
+  FormControl,
+  TextField,
+  Button,
+  Grid,
+  CircularProgress,
+  LinearProgress
+} from "@material-ui/core";
 import { loadReCaptcha, ReCaptcha } from "react-recaptcha-v3";
 import axios from "axios";
+import { GlobalContext } from "../GlobalState";
 
 let captchaToken;
 const FeedbackForm = () => {
+  const { setSnackbarMsg } = React.useContext(GlobalContext);
+  const [isSending, setIsSending] = React.useState(false);
   const formEl = React.useRef(null);
-
   const submitForm = e => {
+    // set state to sending
+    setIsSending(true);
     const formData = new FormData(formEl.current);
     const name = formData.get("name");
     const email = formData.get("email");
@@ -25,16 +36,27 @@ const FeedbackForm = () => {
     axios
       .post("https://xmailerr.glitch.me/contact", post)
       .then(function(response) {
-        console.log(response);
+        console.log(response.data.status);
+        // also clear the form
+        formEl.current.reset();
+        setSnackbarMsg(response.data.status);
+        setIsSending(false);
       })
       .catch(function(error) {
-        console.log(error);
+        // console.log(error.response.status);
+        if (error.response) {
+          if (error.response.status === 429) {
+            formEl.current.reset();
+
+            setSnackbarMsg("We accept limited feedback!");
+          }
+        }
+        setIsSending(false);
       });
   };
 
   React.useEffect(() => {
     loadReCaptcha("6Le1toEUAAAAAITyNwqEMaz3hFAYzciSJDMomrgN");
-    
   }, []);
 
   const verifyCallback = token => {
@@ -98,14 +120,24 @@ const FeedbackForm = () => {
         color="primary"
         required
       />
-
+      {isSending ? (
+        <LinearProgress
+          style={{
+            width: "100%",
+            transform: "translateY(-12px)",
+            borderRadius: "2px"
+          }}
+        />
+      ) : null}
       <Button
+      style={{marginTop: "10px"}}
         variant="outlined"
         color="primary"
         onClick={submitForm}
         type="submit"
       >
-        Submit
+        {/* if sending is true then show circular progress */}
+        {isSending ? "Sending Feedback" : "Send Feedback"}
       </Button>
     </Grid>
   );
